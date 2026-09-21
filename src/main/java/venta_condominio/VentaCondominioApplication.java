74ffc1d98bfaa6a90e578be0a1385d25ff58a690
Santiago Guevara <santiguevar420@gmail.com>
2026-09-21 14:37:12 -0500

    chore: inicializa proyecto Spring Boot

---
+++ src/main/java/venta_condominio/VentaCondominioApplication.java
@@ -0,0 +1,13 @@
+package venta_condominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaCondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaCondominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/venta_condominio/VentaCondominioApplicationTests.java
@@ -0,0 +1,13 @@
+package venta_condominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaCondominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
