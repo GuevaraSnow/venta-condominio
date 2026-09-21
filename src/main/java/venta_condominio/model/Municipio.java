7029ef1d77793e5c045d113d27466eed50f3d3bb
Samuel Calle <samuels.calles@uqvirtual.edu.co>
2026-09-21 17:28:56 -0500

    Revert "feat: valida datos de municipio"
    This reverts commit 7c6f6173da93b0a0fce23c1a6b11cd92438ab78a.

--- src/main/java/venta_condominio/model/Municipio.java
+++ src/main/java/venta_condominio/model/Municipio.java
@@ -6,15 +6,6 @@ public class Municipio {
     private final Departamento departamento;
 
     public Municipio(String nombre, Departamento departamento) {
-
-        if (nombre == null || nombre.isBlank()) {
-            throw new IllegalArgumentException("El nombre del municipio es obligatorio");
-        }
-
-        if (departamento == null) {
-            throw new IllegalArgumentException("El departamento es obligatorio");
-        }
-
         this.nombre = nombre;
         this.departamento = departamento;
     }
