7c6f6173da93b0a0fce23c1a6b11cd92438ab78a
Samuel Calle <samuels.calles@uqvirtual.edu.co>
2026-09-21 17:23:42 -0500

    feat: agrega representacion de Municipio

--- src/main/java/venta_condominio/model/Municipio.java
+++ src/main/java/venta_condominio/model/Municipio.java
@@ -6,6 +6,15 @@ public class Municipio {
     private final Departamento departamento;
 
     public Municipio(String nombre, Departamento departamento) {
+
+        if (nombre == null || nombre.isBlank()) {
+            throw new IllegalArgumentException("El nombre del municipio es obligatorio");
+        }
+
+        if (departamento == null) {
+            throw new IllegalArgumentException("El departamento es obligatorio");
+        }
+
         this.nombre = nombre;
         this.departamento = departamento;
     }
