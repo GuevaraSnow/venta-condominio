727d74aa67759fd7fdf0e09b8fb8c357de37fb0f
OneHit27 <juandatorres18@gmail.com>
2026-09-21 19:47:53 -0500

    feat: crea repositorio de Cliente

---
+++ src/main/java/venta_condominio/repository/ClienteRepositorio.java
@@ -0,0 +1,13 @@
+package venta_condominio.repository;
+
+import java.util.List;
+
+import venta_condominio.model.Cliente;
+
+public interface ClienteRepositorio {
+
+    void guardar(Cliente cliente);
+
+    List<Cliente> listar();
+
+}
