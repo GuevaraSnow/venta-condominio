a3bf98e3dbb4f8b4714322cb90b2c6583e13f6f9
Santiago Guevara <santiguevar420@gmail.com>
2026-09-22 09:53:31 -0500

    feat: agrega validaciones al registro de usuario

--- src/main/java/venta_condominio/service/UsuarioService.java
+++ src/main/java/venta_condominio/service/UsuarioService.java
@@ -1,18 +1,23 @@
 package venta_condominio.service;
 
 import org.springframework.stereotype.Service;
+import venta_condominio.exception.UsuarioException;
 import venta_condominio.model.Rol;
 import venta_condominio.model.Usuario;
 import venta_condominio.repository.UsuarioRepositorio;
-import venta_condominio.exception.UsuarioException;
 
 @Service
 public class UsuarioService {
 
     private final UsuarioRepositorio usuarioRepositorio;
+    private final VerificacionEmailService verificacionEmailService;
 
-    public UsuarioService(UsuarioRepositorio usuarioRepositorio) {
+    public UsuarioService(
+            UsuarioRepositorio usuarioRepositorio,
+            VerificacionEmailService verificacionEmailService) {
+
         this.usuarioRepositorio = usuarioRepositorio;
+        this.verificacionEmailService = verificacionEmailService;
     }
 
     public void registrarUsuario(
@@ -28,6 +33,14 @@ public class UsuarioService {
         if (usuarioExistente != null) {
             throw new UsuarioException(
                     "Ya existe un usuario registrado con ese correo"
+            );
+        }
+
+        if (rol == Rol.CLIENTE &&
+                !verificacionEmailService.estaVerificado(email)) {
+
+            throw new UsuarioException(
+                    "El correo electrónico debe ser verificado antes de crear la cuenta"
             );
         }
 
